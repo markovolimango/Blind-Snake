348586f132d302f69f7ed20a0a44ead822b2d139
markovolimango <maremare005@gmail.com>
2024-12-20 22:02:17 +0100

    all new

--- path.c
+++ path.c
@@ -1,119 +1,97 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define NMAX 2000
-
-void move(int *x, int *y, char dir) {
-    switch (dir) {
-        case 'r':
-            *x = *x + 1;
-            break;
-        case 'l':
-            *x = *x - 1;
-            break;
-        case 'u':
-            *y = *y + 1;
-            break;
-        case 'd':
-            *y = *y - 1;
-            break;
-    }
-}
-
-bool contains(int a[], int x, int n) {
-    for (int i = 0; i < n; i++) {
-        if (a[i] == x)
-            return true;
-    }
-    return false;
+char invert(char c) {
+    if (c == 'd')
+        return 'u';
+    if (c == 'r')
+        return 'l';
+    if (c == 'u')
+        return 'd';
+    if (c == 'l')
+        return 'r';
+    fprintf(stderr, "Invalid character %c\n", c);
+    return 0;
 }
 
 void generate_path(FILE *path, int S) {
-    long long count = 0;
-    int x = 1, y = S;
-    int in_length = 0, in[NMAX], out_length = 0, out[NMAX];
-    char last_move = '/';
+    char *moves_dir = malloc(2 * S * sizeof(char));
+    int *moves_count = malloc(2 * S * sizeof(int));
+
+    if (!moves_dir || !moves_count) {
+        fprintf(stderr, "Memory allocation failed\n");
+        free(moves_dir);
+        free(moves_count);
+        return;
+    }
+
+    int x = 1, y = S, n = 0;
+    char last_move = 'd';
+    int move_count = 0;
+
     while (x != S) {
-        count++;
         if (x + 1 <= S / y) {
-            if (last_move == 'd') {
-                in[in_length++] = y - x;
-            }
             x++;
-            last_move = 'r';
-            fprintf(path, "r");
-        } else {
-            if (last_move == 'r') {
-                out[out_length++] = y - x;
+            if (last_move == 'd') {
+                moves_dir[n] = last_move;
+                moves_count[n] = move_count;
+                n++;
+                fprintf(path, "%c%d", 'd', move_count);
+                move_count = 1;
+                last_move = 'r';
+            } else {
+                move_count++;
             }
+        } else {
             y--;
-            last_move = 'd';
-            fprintf(path, "d");
+            if (last_move == 'r') {
+                moves_dir[n] = last_move;
+                moves_count[n] = move_count;
+                n++;
+                fprintf(path, "%c%d", 'r', move_count);
+                move_count = 1;
+                last_move = 'd';
+            } else {
+                move_count++;
+            }
+        }
+    }
+    moves_dir[n] = last_move;
+    moves_count[n] = move_count;
+    n++;
+    fprintf(path, "%c%d", last_move, move_count);
+
+    int start = 0, end = n;
+    while (start < end) {
+        fprintf(path, "%c%d", invert(moves_dir[end - 1]), moves_count[end - 1] + 1);
+        end -= 2;
+        start += 2;
+        for (int i = end - 1; i >= start; i--) {
+            fprintf(path, "%c%d", invert(moves_dir[i]), moves_count[i]);
+        }
+        if (start >= end)
+            break;
+        fprintf(path, "%c%d", moves_dir[start], moves_count[start] + 1);
+        start += 2;
+        end -= 2;
+        for (int i = start; i < end; i++) {
+            fprintf(path, "%c%d", moves_dir[i], moves_count[i]);
         }
     }
 
-    bool going_up = true, bonus = true;;
-    x--;
-    last_move = 'l';
-    int in_start = 0, in_end = in_length, out_start = 0, out_end = out_length;
-    int i = in_end - 1, j = out_end - 1;
-    while (x != 1 || y != 1) {
-        count++;
-        fputc(last_move, path);
-        if (going_up) {
-            if (contains(in, y - x, in_length)) {
-                if (!bonus) {
-                    y++;
-                    last_move = 'u';
-                } else {
-                    bonus = false;
-                    x--;
-                    last_move = 'l';
-                }
-            } else if (contains(out, y - x, out_length)) {
-                if (x == 1) {
-                    going_up = false;
-                    bonus = true;
-                    y--;
-                    last_move = 'd';
-                    continue;
-                }
-                x--;
-                last_move = 'l';
-            } else {
-                move(&x, &y, last_move);
-            }
-        } else {
-            if (contains(in, y - x, in_length)) {
-                if (!bonus) {
-                    x++;
-                    last_move = 'r';
-                } else {
-                    bonus = false;
-                    y--;
-                    last_move = 'd';
-                }
-            } else if (contains(out, y - x, out_length)) {
-                if (y == 1) {
-                    going_up = true;
-                    bonus = true;
-                    x--;
-                    last_move = 'l';
-                    continue;
-                }
-                y--;
-                last_move = 'd';
-            } else {
-                move(&x, &y, last_move);
-            }
-        }
-    }
-    fputc(last_move, path);
-    printf("%lld\n", count);
+    free(moves_dir);
+    free(moves_count);
 }
 
 int main(void) {
     FILE *path = fopen("path.txt", "w");
-    int S = 25;
+    if (!path) {
+        fprintf(stderr, "Failed to open file\n");
+        return 1;
+    }
+
+    int S = 16;
     generate_path(path, S);
+    fclose(path);
     return 0;
 }
